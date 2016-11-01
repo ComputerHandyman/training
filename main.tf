c9454fd47aaca611a5bfe438df14dc409c0ece0d
HashiCorp Training <training@hashicorp.com>
2016-11-01 15:14:48 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,61 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your subnet ID is:
+#
+#     subnet-d56fbc8e
+#
+# Your security group ID is:
+#
+#     sg-57e40c2a
+#
+# Your AMI ID is:
+#
+#     ami-c9580bde
+#
+# Your Identity is:
+#
+#     datapipe-dragonfly
+#
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-c9580bde"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-57e40c2a"]
+  subnet_id              = "subnet-d56fbc8e"
+  count			 = 2
+
+  tags {
+    Identify = "datapipe-dragonfly"
+  }
+
+  tags {
+    Identify2 = "datapipe-dragonfly2"
+  }
+
+  tags {
+    Identify3 = "datapipe-dragonfly3"
+  }
+}
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
